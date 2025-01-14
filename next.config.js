d887bf2b49f9a2c7703197dd0a4a304f103811f0
JR0-7 <jasirjaleel777@gmail.com>
2025-01-14 15:33:18 +0530

    remove:basePath

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  basePath: '/rockstart_gaming',
+  // basePath: '/rockstart_gaming',
 };
 
 module.exports = nextConfig;
